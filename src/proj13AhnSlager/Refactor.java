154f376b312b46d25150aeec6a6701e904575dfd
Kwahn20 <kevin.w.ahn@gmail.com>
2019-03-04 14:42:59 -0500

    Added in comments into all new files

--- src/proj13AhnSlager/Refactor.java
+++ src/proj13AhnSlager/Refactor.java
@@ -1,3 +1,10 @@
+/*
+ * File: Refactor.java
+ * Names: Kevin Ahn, Kyle Slager
+ * Class: CS461
+ * Project 13
+ */
+
 package proj13AhnSlager;
 
 import javafx.scene.Scene;
@@ -11,13 +18,13 @@ import proj13AhnSlager.bantam.semant.*;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import javafx.scene.control.ListView;
 
 /**
  * @author Kevin Ahn, Kyle Slager
- * A refactoring class that sets up a window
+ * A refactoring class that sets up a window that sets up the UI to display the
+ * classes, methods ands fields of a class to refactor them if the user decides to
  */
-import javafx.scene.control.ListView;
-
 public class Refactor {
 
     private Iterator<int[]> indices = new ArrayList<int[]>().iterator();
@@ -31,11 +38,22 @@ public class Refactor {
     private Button fieldButton;
     private TextField userEntryTextField;
 
+
+    /**
+     * constructor for the class
+     * @param editController editController to be used by the class
+     * @param parseRoot the program ast
+     */
     public Refactor(EditController editController, Program parseRoot){
         this.editController = editController;
         this.parseRoot = parseRoot;
     }
 
+    /**
+     * a method that initializes the window that will hold the information from the AST.
+     * Creates a set of buttons that will redirect the user to the list of classes, methods,
+     * or fields
+     */
     public void initialize(){
         CodeArea currentCodeArea = this.editController.getCurJavaCodeArea();
         Stage popupWindow  = new Stage();
@@ -60,9 +78,14 @@ public class Refactor {
         popupWindow.showAndWait();
     }
 
+    /**
+     * method to get the list of classes in the current file, creates a new ClassVisitor
+     * to get the list of name and adds them to an arrayList.
+     * @param window the current window
+     */
     public void getClasses(Stage window){
         window.close();
-        ClassVisitor classVisitor = new ClassVisitor();
+        ClassVisitor classVisitor = new ClassVisitor(); // creates the visitor
         ArrayList<String> names = classVisitor.getClasses(this.parseRoot);
         CodeArea currentCodeArea = this.editController.getCurJavaCodeArea();
         Stage popupWindow  = new Stage();
@@ -80,9 +103,14 @@ public class Refactor {
 
     }
 
+    /**
+     * method to get the list of methods in the current file, creates a new MethodVisitor
+     * to get the list of names and adds them to an arrayList.
+     * @param window the current window
+     */
     public void getMethods(Stage window){
         window.close();
-        MethodVisitor methodVisitor = new MethodVisitor();
+        MethodVisitor methodVisitor = new MethodVisitor(); // creates the method visitor
         ArrayList<String> names = methodVisitor.getMethods(this.parseRoot);
         CodeArea currentCodeArea = this.editController.getCurJavaCodeArea();
         Stage popupWindow  = new Stage();
@@ -99,6 +127,11 @@ public class Refactor {
         popupWindow.showAndWait();
     }
 
+    /**
+     * method to get the list of fields in the current file, creates a new FieldVisitor
+     * to get the list of names and adds them to an arrayList.
+     * @param window the current window
+     */
     public void getFields(Stage window){
         window.close();
         FieldVisitor fieldVisitor = new FieldVisitor();
--- src/proj13AhnSlager/bantam/semant/ClassVisitor.java
+++ src/proj13AhnSlager/bantam/semant/ClassVisitor.java
@@ -1,3 +1,10 @@
+/*
+ * File: ClassVisitor.java
+ * Names: Kevin Ahn, Kyle Slager
+ * Class: CS461
+ * Project 13
+ */
+
 package proj13AhnSlager.bantam.semant;
 
 import proj13AhnSlager.bantam.ast.ASTNode;
@@ -8,19 +15,35 @@ import proj13AhnSlager.bantam.visitor.Visitor;
 
 import java.util.ArrayList;
 
+/**
+ * @author Kevin Ahn and Kyle Slager
+ * ClassVisitor class that extends the Visitor class to get the list of classes from
+ * an AST and add it to an arrayList
+ */
 public class ClassVisitor extends Visitor {
 
     public ArrayList classList;
 
+    /**
+     * method that creates a new class list and calls the parseRoots accepts method
+     * @param parseRoot
+     * @return an ArrayList of class names
+     */
     public ArrayList<String> getClasses(Program parseRoot){
         classList = new ArrayList();
         parseRoot.accept(this);
         return classList;
     }
 
+    /**
+     * overrides the Class_ visit method to add in the names of each class in the AST
+     * @param node the class node
+     * @return
+     */
+    @Override
     public Object visit(Class_ node){
-        System.out.println(node.getName());
-        classList.add(node.getName());
+        System.out.println(node.getName()); // used for testing
+        classList.add(node.getName()); // adds it to the class list
         node.getMemberList().accept(this);
         return null;
     }
